55b904697a1ca5d74dfb74e3cbf90e6192c7ba56
anton <anton@vayosoft.com>
2022-11-03 13:33:25 +0200

    add gRPC Handler

--- EmulatorRC.Client/Program.cs
+++ EmulatorRC.Client/Program.cs
@@ -7,43 +7,43 @@ using Grpc.Net.Client;
 var channel = GrpcChannel.ForAddress("http://localhost:5001");
 var client = new Screener.ScreenerClient(channel);
 
-foreach (var i in Enumerable.Range(0, 10))
-{
-    var response = await client.GetScreenAsync(new ScreenRequest() { Id = $"ID_{i}" });
-    Console.WriteLine(response.Image);
-    await Task.Delay(1000);
-}
-
-var screen = client.GetScreenStream(new ScreenRequest() { Id = "1" });
-await foreach (var response in screen.ResponseStream.ReadAllAsync())
-{
-    Console.WriteLine("Screen: " + response.Image);
-}
-
-
-//Console.WriteLine("Starting background task to receive messages");
-//var readTask = Task.Run(async () =>
+//foreach (var i in Enumerable.Range(0, 2))
 //{
-//    await foreach (var response in call.ResponseStream.ReadAllAsync())
-//    {
-//        Console.WriteLine(response.Message);
-//        // Echo messages sent to the service
-//    }
-//});
-
-//Console.WriteLine("Starting to send messages");
-//Console.WriteLine("Type a message to echo then press enter.");
-//while (true)
-//{
-//    var result = Console.ReadLine();
-//    if (string.IsNullOrEmpty(result))
-//    {
-//        break;
-//    }
-
-//    await call.RequestStream.WriteAsync(new HelloRequest { Name = result });
+//    var response = await client.GetScreenAsync(new ScreenRequest() { Id = $"ID_{i}" });
+//    Console.WriteLine("Screen: " + response.Image.ToStringUtf8());
+//    await Task.Delay(1000);
 //}
 
-//Console.WriteLine("Disconnecting");
-//await call.RequestStream.CompleteAsync();
-//await readTask;
+//using var screen = client.GetScreenStream(new ScreenRequest() { Id = "1" });
+//await foreach (var response in screen.ResponseStream.ReadAllAsync())
+//{
+//    Console.WriteLine("ScreenStream: " + response.Image.ToStringUtf8());
+//}
+
+//***************************************************************************************
+using var call = client.GetScreen2Stream();
+Console.WriteLine("Starting background task to receive messages");
+var readTask = Task.Run(async () =>
+{
+    await foreach (var response in call.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine("Screen2Stream: " + response.Image.ToStringUtf8());
+    }
+});
+
+Console.WriteLine("Starting to send messages");
+Console.WriteLine("Type a message to echo then press enter.");
+while (true)
+{
+    var result = Console.ReadLine();
+    if (string.IsNullOrEmpty(result))
+    {
+        break;
+    }
+
+    await call.RequestStream.WriteAsync(new ScreenRequest() { Id = "1" });
+}
+
+Console.WriteLine("Disconnecting");
+await call.RequestStream.CompleteAsync();
+await readTask;
--- src/EmulatorRC.API/Services/ScreenService.cs
+++ src/EmulatorRC.API/Services/ScreenService.cs
@@ -1,17 +1,21 @@
-﻿using EmulatorRC.API.Protos;
+﻿using System.Text;
+using EmulatorRC.API.Extensions;
+using EmulatorRC.API.Protos;
+using EmulatorRC.Services;
 using Google.Protobuf;
 using Grpc.Core;
-using LanguageExt;
 
 namespace EmulatorRC.API.Services
 {
     public class ScreenService : Screener.ScreenerBase
     {
         private readonly ILogger<ScreenService> _logger;
+        private readonly IEmulatorDataRepository _emulatorDataRepository;
 
-        public ScreenService(ILogger<ScreenService> logger)
+        public ScreenService(ILogger<ScreenService> logger, IEmulatorDataRepository emulatorDataRepository)
         {
             _logger = logger;
+            _emulatorDataRepository = emulatorDataRepository;
         }
 
         public override Task<ScreenReply> GetScreen(ScreenRequest request, ServerCallContext context)
@@ -19,20 +23,47 @@ namespace EmulatorRC.API.Services
             _logger.LogInformation("Saying hello to {Name}", request.Id);
             return Task.FromResult(new ScreenReply
             {
-               Image = ByteString.CopyFrom(Array.Empty<byte>())
+               Image = ByteString.CopyFrom(Encoding.UTF8.GetBytes("GetScreen"))
             });
         }
 
         public override async Task GetScreenStream(ScreenRequest request, IServerStreamWriter<ScreenReply> responseStream, ServerCallContext context)
         {
-            foreach (var x in Enumerable.Range(1, 10))
+            foreach (var x in Enumerable.Range(1, 2))
             {
                 await responseStream.WriteAsync(new ScreenReply
                 {
-                    Image = ByteString.CopyFrom(Array.Empty<byte>())
+                    Image = ByteString.CopyFrom(Encoding.UTF8.GetBytes("GetScreenStream"))
                 });
 
                 await Task.Delay(200);
+            }
+        }
+
+        public override async Task GetScreen2Stream(IAsyncStreamReader<ScreenRequest> requestStream, IServerStreamWriter<ScreenReply> responseStream, ServerCallContext context)
+        {
+            var httpContext = context.GetHttpContext();
+            var deviceId = httpContext.Request.GetDeviceIdOrDefault();
+
+            _logger.LogInformation("Connected for {deviceId}", deviceId);
+
+            while (await requestStream.MoveNext() && !context.CancellationToken.IsCancellationRequested)
+            {
+                var id = requestStream.Current.Id;
+                Console.WriteLine("Stream request " + id);
+
+                byte[]? bytes = null;
+                if (deviceId is not null)
+                {
+                    bytes = _emulatorDataRepository.GetLastScreen(deviceId);
+                }
+
+                var response = new ScreenReply
+                {
+                    Image = ByteString.CopyFrom(bytes ?? Array.Empty<byte>())
+                };
+
+                await responseStream.WriteAsync(response);
             }
         }
     }
