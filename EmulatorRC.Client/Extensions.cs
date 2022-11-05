93347dad0695ba9a01debb45f2a4bdd5a1d83e6d
dravgust <dravgust@hotmail.com>
2022-11-05 17:24:19 +0200

    grpc

---
+++ EmulatorRC.Client/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace EmulatorRC.Client
+{
+    public static class Extensions
+    {
+        public static TaskAwaiter GetAwaiter(this TimeSpan time)
+        {
+            return Task.Delay(time).GetAwaiter();
+        }
+
+        public static TaskAwaiter GetAwaiter(this int milliseconds)
+        {
+            return Task.Delay(milliseconds).GetAwaiter();
+        }
+
+        public static TimeSpan Seconds(this int seconds)
+        {
+            return TimeSpan.FromSeconds(seconds);
+        }
+    }
+}
