c9e2651079a3ab1ed0cbe5d53ff76309d05fb92d
anton <anton@vayosoft.com>
2022-11-16 13:13:54 +0200

    .

--- data/EmulatorHub.MySqlMigrations/DesignTimeContextFactory.cs
+++ data/EmulatorHub.MySqlMigrations/DesignTimeContextFactory.cs
@@ -1,6 +1,7 @@
 ﻿using EmulatorHub.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace EmulatorHub.MySqlMigrations
 {
@@ -8,11 +9,14 @@ namespace EmulatorHub.MySqlMigrations
     {
         public HubDbContext CreateDbContext(string[] args)
         {
+            var builder = new ConfigurationBuilder();
+            builder.AddCommandLine(args);
+            var config = builder.Build();
+
+            var connectionString = config["cs"] ?? "Server=localhost;Port=3306;Database=emurc;Uid=root;Pwd=1q2w3e4r;";
+            var serverVersion = new MySqlServerVersion(config["ver"] ?? "8.0.25");
+
             var dbContextOptions = new DbContextOptionsBuilder();
-
-            var connectionString = "Server=localhost;Port=3306;Database=emurc;Uid=root;Pwd=1q2w3e4r;";
-            var serverVersion = new MySqlServerVersion(new Version(8, 0, 25));
-
             dbContextOptions.UseMySql(connectionString, serverVersion, 
                 b => b.MigrationsAssembly("EmulatorHub.MySqlMigrations"));
 
