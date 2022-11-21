806d38ed377017ae82d7be7654461f457fe0ed00
anton <anton@vayosoft.com>
2022-11-21 10:41:38 +0200

    .

---
+++ src/EmulatorHub.Infrastructure/Persistence/Filters/QueryFilters.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Vayosoft.Commons.Entities;
+using Vayosoft.Identity;
+using Vayosoft.Identity.Extensions;
+
+namespace EmulatorHub.Infrastructure.Persistence.Filters
+{
+    //foreach (var mutableEntityType in modelBuilder.Model.GetEntityTypes())
+    //{
+    //    if (typeof(ISoftDelete).IsAssignableFrom(mutableEntityType.ClrType)) 
+    //        mutableEntityType.AddSoftDeleteQueryFilter();
+
+    //    if (_userContext != null)
+    //    {
+    //        if (typeof(IProviderId).IsAssignableFrom(mutableEntityType.ClrType))
+    //            mutableEntityType.AddProviderIdQueryFilter(_userContext);
+    //    }
+    //}
+
+    public enum QueryFilterTypes { SoftDelete, ProviderId }
+    public static class QueryFilters
+    {
+        public static void AddSoftDeleteQueryFilter(this IMutableEntityType entityData)
+        {
+            var methodToCall = GetMethodToCall(entityData, QueryFilterTypes.SoftDelete);
+            var filter = methodToCall.Invoke(null, new object[] { })!;
+            entityData.SetQueryFilter((LambdaExpression)filter);
+            entityData.AddIndex(entityData.FindProperty(nameof(ISoftDelete.SoftDeleted))!);
+        }
+
+        public static void AddProviderIdQueryFilter(this IMutableEntityType entityData, IUserContext userContext)
+        {
+            var methodToCall = GetMethodToCall(entityData, QueryFilterTypes.ProviderId);
+
+            var filter = methodToCall.Invoke(null, new object[] { userContext })!;
+            entityData.SetQueryFilter((LambdaExpression)filter);
+            entityData.AddIndex(entityData.FindProperty((nameof(IProviderId.ProviderId)))!);
+        }
+
+        private static MethodInfo GetMethodToCall(IReadOnlyTypeBase entityData, QueryFilterTypes queryFilterType)
+        {
+            var methodName = $"Get{queryFilterType}Filter";
+
+            return typeof(QueryFilters)
+                .GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Static)!
+                .MakeGenericMethod(entityData.ClrType);
+        }
+
+        private static LambdaExpression GetProviderIdFilter<TEntity>(IUserContext userContext)
+            where TEntity : class, IProviderId
+        {
+            Expression<Func<TEntity, bool>> filter = x => (long)x.ProviderId == userContext.User.Identity.GetProviderId();
+            return filter;
+        }
+
+        private static LambdaExpression GetSoftDeleteFilter<TEntity>(IUserContext userContext)
+            where TEntity : class, ISoftDelete
+        {
+            Expression<Func<TEntity, bool>> filter = x => !x.SoftDeleted;
+            return filter;
+        }
+
+    }
+}
