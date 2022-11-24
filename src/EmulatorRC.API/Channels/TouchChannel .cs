fe339affc20fa55724965412157050322d4ea85b
dravgust <dravgust@hotmail.com>
2022-11-24 21:39:02 +0200

    fix get channel

--- src/EmulatorRC.API/Channels/TouchChannel .cs
+++ src/EmulatorRC.API/Channels/TouchChannel .cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Concurrent;
 using System.Threading.Channels;
+using System.Xml.Linq;
 using EmulatorRC.API.Protos;
 
 namespace EmulatorRC.API.Channels
@@ -7,6 +8,7 @@ namespace EmulatorRC.API.Channels
     public sealed class TouchChannel : IDisposable
     {
         private readonly ConcurrentDictionary<string, Channel<TouchEvents>> _channels = new();
+        private readonly ConcurrentDictionary<string, object> _locks = new();
 
         private readonly BoundedChannelOptions _options = new(1)
         {
@@ -24,13 +26,18 @@ namespace EmulatorRC.API.Channels
             }
         }
 
-        
         public IAsyncEnumerable<TouchEvents> ReadAllAsync(string deviceId, CancellationToken cancellationToken = default)
         {
             if (!_channels.TryGetValue(deviceId, out var channel))
             {
-                channel = Channel.CreateBounded<TouchEvents>(_options);
-                _channels.TryAdd(deviceId, channel);
+                lock (_locks.GetOrAdd(deviceId, s => new object()))
+                {
+                    if (!_channels.TryGetValue(deviceId, out channel))
+                    {
+                        channel = Channel.CreateBounded<TouchEvents>(_options);
+                        _channels.TryAdd(deviceId, channel);
+                    }
+                }
             }
 
             return channel.Reader.ReadAllAsync(cancellationToken);
