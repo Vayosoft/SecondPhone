3f62660e67df1b8ebf375233e6f29d18c3bd7708
dravgust <dravgust@hotmail.com>
2022-11-26 13:25:00 +0200

    .

---
+++ tests/SecondPhone.Benchmarks/Benchmarks.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using EmulatorHub.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EmulatorRC.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        private static UserService UserService { get; }
+
+        static Benchmarks()
+        {
+            var host = new HostBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddHttpClient();
+                    services.AddTransient<UserService>();
+                })
+                .Build();
+
+            UserService = host.Services.GetRequiredService<UserService>();
+        }
+
+
+    }
+}
