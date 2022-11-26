b4084655be42b1187e2c70a943b260746675228f
dravgust <dravgust@hotmail.com>
2022-11-26 13:40:34 +0200

    .

---
+++ src/EmulatorHub/Application/Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using Vayosoft.Identity;
+
+namespace EmulatorHub.Application.Services
+{
+    public class UserService
+    {
+        private readonly HttpClient _httpClient;
+
+        public UserService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        public Task<UserEntity> GetUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
