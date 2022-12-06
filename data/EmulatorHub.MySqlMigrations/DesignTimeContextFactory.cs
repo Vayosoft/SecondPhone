4ff363eb6e3876c01cfef1af87b0602e02b4c76d
anton <anton@vayosoft.com>
2022-12-06 14:53:06 +0200

    identity

--- data/EmulatorHub.MySqlMigrations/DesignTimeContextFactory.cs
+++ data/EmulatorHub.MySqlMigrations/DesignTimeContextFactory.cs
@@ -17,7 +17,7 @@ namespace EmulatorHub.MySqlMigrations
             //var connectionString = config["cs"] ?? "Server=192.168.10.11;Port=3306;Database=emurc;Uid=admin;Pwd=~1q2w3e4r!";
             var serverVersion = new MySqlServerVersion(config["ver"] ?? "8.0.25");
 
-            var dbContextOptions = new DbContextOptionsBuilder();
+            var dbContextOptions = new DbContextOptionsBuilder<HubDbContext>();
             dbContextOptions.UseMySql(connectionString, serverVersion, 
                 b => b.MigrationsAssembly("EmulatorHub.MySqlMigrations"))
 
