27aaeefbf4703c320eff723eb8ef5c4dc7c0763d
anton <anton@vayosoft.com>
2022-12-22 12:23:45 +0200

    .

---
+++ src/EmulatorRC.API/Handlers/InnerHandler.cs
@@ -0,0 +1,152 @@
+﻿using EmulatorRC.API.Channels;
+using EmulatorRC.Entities;
+using Microsoft.AspNetCore.Connections;
+using System.Buffers;
+using System.IO.Pipelines;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EmulatorRC.API.Handlers
+{
+    public class InnerHandler : ConnectionHandler
+    {
+        private readonly StreamChannel _channel;
+        private readonly ILogger<InnerHandler> _logger;
+        private readonly IHostApplicationLifetime _lifetime;
+
+        public InnerHandler(
+            StreamChannel channel,
+            ILogger<InnerHandler> logger,
+            IHostApplicationLifetime lifetime)
+        {
+            _channel = channel;
+            _logger = logger;
+            _lifetime = lifetime;
+        }
+
+        public override async Task OnConnectedAsync(ConnectionContext connection)
+        {
+            _logger.LogInformation("{connectionId} connected", connection.ConnectionId);
+
+            try
+            {
+                var cts = CancellationTokenSource.CreateLinkedTokenSource(
+                    connection.ConnectionClosed, _lifetime.ApplicationStopping);
+                var token = cts.Token;
+
+                DeviceSession session = null;
+
+                while (!token.IsCancellationRequested)
+                {
+                    var result = await connection.Transport.Input.ReadAsync(token);
+                    var buffer = result.Buffer;
+
+                    if (session == null)
+                    {
+                        session = Handshake(ref buffer, connection.Transport.Output);
+                        _ = ReadFromChannelAsync(session.DeviceId, connection.Transport.Output, token);
+                    }
+
+                    if (result.IsCompleted)
+                    {
+                        break;
+                    }
+
+                    connection.Transport.Input.AdvanceTo(buffer.End);
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "{connectionId} => {error}", connection.ConnectionId, e.Message);
+            }
+
+            _logger.LogInformation("{connectionId} disconnected", connection.ConnectionId);
+        }
+
+        public async Task ReadFromChannelAsync(string deviceId, PipeWriter output, CancellationToken token)
+        {
+            while (true)
+            {
+                if (_channel.TryGetChannel(deviceId, out var channel))
+                {
+                    var result = await channel.Reader.ReadAsync(token);
+                    var buffer = result.Buffer;
+
+                    foreach (var segment in buffer)
+                    {
+                        await output.WriteAsync(segment, token);
+                    }
+
+                    if (result.IsCompleted)
+                    {
+                        break;
+                    }
+
+                    channel.Reader.AdvanceTo(buffer.End);
+                }
+                else
+                {
+                    await Task.Delay(1000, token);
+                }
+            }
+        }
+
+        private const RegexOptions RegexOptions =
+            System.Text.RegularExpressions.RegexOptions.Compiled |
+            System.Text.RegularExpressions.RegexOptions.IgnoreCase |
+            System.Text.RegularExpressions.RegexOptions.Singleline;
+        private static DeviceSession Handshake(ref ReadOnlySequence<byte> buffer, PipeWriter output)
+        {
+            var payload = Encoding.UTF8.GetString(buffer.First.Span);
+            if (payload.StartsWith("CMD /v2/video.4?"))
+            {
+                var s = payload.Split("?")[1];
+
+                var m = Regex.Match(s, "(\\d+)x(\\d+)&id=(\\w+)", RegexOptions);
+                if (!m.Success || m.Groups.Count < 4)
+                    throw new Exception("Authentication required");
+
+                if (!int.TryParse(m.Groups[1].Value, out var w) || !int.TryParse(m.Groups[2].Value, out var h))
+                    throw new Exception("Authentication required");
+
+                var deviceId = m.Groups[3].Value;
+
+                if (w == 0 || h == 0 || string.IsNullOrEmpty(deviceId))
+                    throw new Exception("Authentication required");
+
+                buffer = buffer.Slice(buffer.End);
+
+                output.WriteAsync(CreateMockHeader(w, h));
+
+                return new DeviceSession { DeviceId = deviceId, StreamType = "cam" };
+            }
+
+            throw new Exception("Authentication required");
+        }
+
+        private static byte[] CreateMockHeader(int width, int height)
+        {
+            const int some1 = 0x21; //25,
+            const int some2 = 0x307fe8f5;
+
+            var byteBuffer = new List<byte>(9)
+            {
+                //0x02, 0x80 - 640
+                (byte) (width >> 8 & 255),
+                (byte) (width & 255),
+                //0x01, 0xe0 - 480
+                (byte)(height >> 8 & 255),
+                (byte)(height & 255),
+                //0x21 - 33
+                some1 & 255,
+                //0xf5, 0xe8, 0x7f, 0x30
+                some2 & 255,
+                some2 >> 8 & 255,
+                some2 >> 16 & 255,
+                some2 >> 24 & 255
+            };
+
+            return byteBuffer.ToArray();
+        }
+    }
+}
---
+++ src/EmulatorRC.API/Handlers/OuterHandler.cs
@@ -0,0 +1,143 @@
+﻿using EmulatorRC.API.Channels;
+using EmulatorRC.Entities;
+using Microsoft.AspNetCore.Connections;
+using System.Buffers;
+using System.IO.Pipelines;
+using System.Text.Json;
+
+namespace EmulatorRC.API.Handlers
+{
+    public class OuterHandler : ConnectionHandler
+    {
+        private readonly StreamChannel _channel;
+        private readonly ILogger<OuterHandler> _logger;
+        private readonly IHostApplicationLifetime _lifetime;
+
+        public OuterHandler(
+            StreamChannel channel,
+            ILogger<OuterHandler> logger,
+            IHostApplicationLifetime lifetime)
+        {
+            _channel = channel;
+            _logger = logger;
+            _lifetime = lifetime;
+        }
+
+        public override async Task OnConnectedAsync(ConnectionContext connection)
+        {
+            _logger.LogInformation("{connectionId} connected", connection.ConnectionId);
+
+            try
+            {
+                var cts = CancellationTokenSource.CreateLinkedTokenSource(
+                    connection.ConnectionClosed, _lifetime.ApplicationStopping);
+                var token = cts.Token;
+
+                HandshakeStatus status = default;
+                Pipe channel = default;
+
+                while (true)
+                {
+                    var result = await connection.Transport.Input.ReadAsync(token);
+                    var buffer = result.Buffer;
+
+                    switch (status)
+                    {
+                        case HandshakeStatus.Successful:
+                            {
+                                foreach (var segment in buffer)
+                                {
+                                    await channel!.Writer.WriteAsync(segment, token);
+                                }
+
+                                break;
+                            }
+                        case HandshakeStatus.Pending:
+                            status = Handshake(ref buffer, out var session);
+                            channel = status switch
+                            {
+                                HandshakeStatus.Successful => _channel.GetOrCreateChannel(session.DeviceId),
+                                HandshakeStatus.Failed => throw new Exception("Authentication required"),
+
+                                _ => channel
+                            };
+
+                            break;
+                    }
+
+                    if (result.IsCompleted)
+                    {
+                        break;
+                    }
+
+                    connection.Transport.Input.AdvanceTo(status != HandshakeStatus.Pending ? buffer.End : buffer.Start);
+                }
+
+                await connection.Transport.Input.CompleteAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "{connectionId} => {error}", connection.ConnectionId, e.Message);
+            }
+
+            _logger.LogInformation("{connectionId} disconnected", connection.ConnectionId);
+        }
+
+        private HandshakeStatus Handshake(ref ReadOnlySequence<byte> buffer, out DeviceSession session)
+        {
+            try
+            {
+                if (buffer.IsSingleSegment)
+                {
+                    var span = buffer.FirstSpan;
+                    var length = BitConverter.ToInt32(span[..4]);
+
+                    if (span.Length < length + 4)
+                    {
+                        session = null;
+                        return HandshakeStatus.Pending;
+                    }
+
+                    session = JsonSerializer.Deserialize<DeviceSession>(span.Slice(4, length));
+                    buffer = buffer.Slice(4 + length);
+                }
+                else
+                {
+                    var reader = new SequenceReader<byte>(buffer);
+
+                    if (!reader.TryReadExact(4, out var header))
+                    {
+                        session = null;
+                        return HandshakeStatus.Pending;
+                    }
+
+                    var length = BitConverter.ToInt32(header.FirstSpan);
+                    if (!reader.TryReadExact(length, out var handshake))
+                    {
+                        session = null;
+                        return HandshakeStatus.Pending;
+                    }
+
+                    session = JsonSerializer.Deserialize<DeviceSession>(handshake.FirstSpan);
+                    buffer = buffer.Slice(4 + length);
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Handshake => {error}\r\n", e.Message);
+
+                session = null;
+                return HandshakeStatus.Failed;
+            }
+
+            return HandshakeStatus.Successful;
+        }
+
+        public enum HandshakeStatus : byte
+        {
+            Pending,
+            Successful,
+            Failed
+        }
+    }
+}
