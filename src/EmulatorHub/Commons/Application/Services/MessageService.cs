5f99223296aacb1dd7c33c893f3e1d19a5874457
anton <anton@vayosoft.com>
2022-12-26 08:48:11 +0200

    .

---
+++ src/EmulatorHub/Commons/Application/Services/MessageService.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+
+namespace EmulatorHub.Commons.Application.Services
+{
+    public class MessageService
+    {
+        private readonly HttpClient _httpClient;
+
+        public MessageService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        public async Task<string> SendPushMessage(string deviceId, object message)
+        {
+            var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri("http://localhost:5005/api/messages/push/send"),
+                Method = HttpMethod.Post,
+                
+            };
+            request.Headers.Add("x-device-id", deviceId);
+
+            request.Content = new StringContent(JsonSerializer.Serialize(message), Encoding.UTF8,
+                "application/json");
+
+            var response = await _httpClient.SendAsync(request);
+            var headers = response.ToString();
+            var body = await response.Content.ReadAsStringAsync();
+            return body;
+        }
+    }
+}
---
+++ test/EmulatorHub.IntegrationTests/MessageTests.cs
@@ -0,0 +1,30 @@
+﻿using EmulatorHub.Commons.Application.Services;
+using EmulatorHub.Commons.Application.Services.Commons;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.Abstractions;
+
+namespace EmulatorHub.IntegrationTests
+{
+    public class MessageTests
+    {
+        private readonly MessageService _userService;
+        private readonly ITestOutputHelper _logger;
+
+        public MessageTests(ITestOutputHelper logger)
+        {
+            _logger = logger;
+            _userService = Server.Host.Services.GetRequiredService<MessageService>();
+        }
+
+        [Fact]
+        public async Task Can_Send_PushMessage()
+        {
+           var result = await _userService.SendPushMessage("device_123", new TestPushMessage(Guid.NewGuid().ToString(), "test"));
+
+            _logger.WriteLine(result);
+        }
+
+        public record TestPushMessage(string Id, string Text);
+
+    }
+}
