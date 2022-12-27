78d731d35fcd395e8e58aafa1b9ba23e0409d65d
anton <anton@vayosoft.com>
2022-12-27 14:52:30 +0200

    .

---
+++ src/EmulatorRC.API/Configuration.cs
@@ -0,0 +1,43 @@
+﻿using App.Metrics;
+using App.Metrics.AspNetCore;
+using App.Metrics.Extensions.Configuration;
+using App.Metrics.Filtering;
+using App.Metrics.Formatters.Ascii;
+using App.Metrics.Formatters.Prometheus;
+
+namespace EmulatorRC.API
+{
+    public static class Configuration
+    {
+        public static IServiceCollection AddDiagnostics(this WebApplicationBuilder builder)
+        {
+            var configuration = builder.Configuration;
+
+            var filter = new MetricsFilter()
+                .WhereContext(name => name != "Application.HttpRequests");
+            var metricsBuilder = AppMetrics.CreateDefaultBuilder()
+                .Configuration.ReadFrom(configuration)
+                .Filter.With(filter);
+
+            var metrics = metricsBuilder.Build();
+
+            builder.Services
+                .AddMetrics(metrics);
+
+            builder.Host
+                .UseMetrics(metricsWebHostOptions =>
+                {
+                    metricsWebHostOptions.EndpointOptions = metricEndpointsOptions =>
+                    {
+                        metricEndpointsOptions.MetricsEndpointOutputFormatter = new MetricsTextOutputFormatter();
+                        metricEndpointsOptions.MetricsTextEndpointOutputFormatter =
+                            new MetricsPrometheusTextOutputFormatter();
+
+                        metricEndpointsOptions.EnvironmentInfoEndpointEnabled = false;
+                    };
+                });
+
+            return builder.Services;
+        }
+    }
+}
---
+++ src/EmulatorRC.API/Services/Diagnostics/MetricsRegistry.cs
@@ -0,0 +1,30 @@
+﻿using App.Metrics;
+using App.Metrics.Counter;
+using App.Metrics.ReservoirSampling.Uniform;
+using App.Metrics.Timer;
+
+namespace EmulatorRC.API.Services.Diagnostics
+{
+    public class MetricsRegistry
+    {
+        public static TimerOptions TimerUsingAlgorithmRReservoir = new()
+        {
+            Name = "uniform",
+            Reservoir = () => new DefaultAlgorithmRReservoir()
+        };
+
+        public static CounterOptions ActiveOuterConnections => new()
+        {
+            Context = "OuterHandler",
+            Name = "Active Outer Connections",
+            MeasurementUnit = Unit.Calls
+        };
+
+        public static CounterOptions ActiveInnerConnections => new()
+        {
+            Context = "InnerHandler",
+            Name = "Active Inner Connections",
+            MeasurementUnit = Unit.Calls
+        };
+    }
+}
