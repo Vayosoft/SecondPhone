47c03f86a595d0247507d32bce4ef95ce67b04cb
anton <anton@vayosoft.com>
2023-01-15 09:20:00 +0200

    .

---
+++ src/EmulatorHub/Domain/Commons/Entities/MobileClient.cs
@@ -0,0 +1,17 @@
+﻿using Vayosoft.Commons.Entities;
+using Vayosoft.Identity;
+
+namespace EmulatorHub.Domain.Commons.Entities
+{
+    public class MobileClient : EntityBase<string>, IProviderId<long>, ISoftDelete
+    {
+        public string Name { get; set; }
+        public UserEntity User { get; set; } = null!;
+        public string PushToken { get; set; }
+
+        public long ProviderId { get; set; }
+        object IProviderId.ProviderId => ProviderId;
+
+        public bool SoftDeleted { get; set; }
+    }
+}
