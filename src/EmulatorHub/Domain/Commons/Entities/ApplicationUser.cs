32647da24d1b05ffc0613180a4218dab5238d28d
anton <anton@vayosoft.com>
2023-02-08 11:58:08 +0200

    .

---
+++ src/EmulatorHub/Domain/Commons/Entities/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Vayosoft.Identity;
+
+namespace EmulatorHub.Domain.Commons.Entities
+{
+    public sealed class ApplicationUser : UserEntity
+    {
+        public ApplicationUser(string username)
+            : base(username)
+        {
+
+        }
+
+        public string PushToken { get; set; }
+    }
+}
